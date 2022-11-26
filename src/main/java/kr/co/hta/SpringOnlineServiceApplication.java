4b7befc1fe9fb43c62da9e661c81bbb0dd515f7b
youna <dbskrltjq@gmail.com>
2022-11-26 22:03:42 +0900

    first commit

---
+++ src/main/java/kr/co/hta/SpringOnlineServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOnlineServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOnlineServiceApplication.class, args);
+	}
+
+}
