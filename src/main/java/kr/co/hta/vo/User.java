92ee5e350e0b6e89e52cf12aa41803707b5debd2
youna <dbskrltjq@gmail.com>
2022-11-26 23:46:33 +0900

    myBatis 환경설정파일 & UserMapper.xml 추가

--- src/main/java/kr/co/hta/vo/User.java
+++ src/main/java/kr/co/hta/vo/User.java
@@ -2,9 +2,17 @@ package kr.co.hta.vo;
 
 import java.util.Date;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Data;
 
 @Data
+/*
+ * @Alias
+ * 		- Mapper 파일에서 사용할 별칭을 지정
+ * 		- mybatis의 Mapper설정파일에서 kr.co.hta.vo.User 대신 User로 사용할 수 있게한다.
+ */		
+@Alias("User")	
 public class User {
 
 	private String id;
